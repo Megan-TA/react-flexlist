a84b2f9e70b4d6366bfe2a67c6b577db48da7804
chen_huang <chen_huang@Ctrip.com>
2018-05-28 15:19:05 +0800

    新增下拉上滑刷新组件

---
+++ index.js
@@ -0,0 +1,10 @@
+/*
+ * 入口文件
+ * @Author: chen_huang 
+ * @Date: 2018-05-28 15:17:03 
+ * @Last Modified by: chen_huang
+ * @Last Modified time: 2018-05-28 15:17:30
+ */
+import Index from './lib'
+
+export default Index
